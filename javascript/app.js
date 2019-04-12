74180ddccab9313498a2d82c6d94155888be7fa0
cuongle30 <cuongle30@gmail.com>
2019-04-11 18:41:13 -0700

    add app

---
+++ javascript/app.js
@@ -0,0 +1,19 @@
+document.querySelector(".container").addEventListener("click", function(event) { 
+    if (event.target.tagName == "BUTTON") {
+    // Variable to grab data from inside button
+    var search = event.target.innerText
+    // Constructing a queryURL using the animal name
+    var queryURL = `https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyCSWFckVRIzwf7VdUWd4EFtufS3Ma3Mp0&part=snippet,contentDetails,statistics,status`;
+    
+    var movie = this.dataset.name;
+    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+
+      console.log(queryURL)
+    // Performing a request with the queryURL
+    fetch(queryURL, {
+      method: "GET"
+    })
+      // After data comes back from the request
+      .then(function(response) { return response.json() })
+      .then(function(response) {
+        console.log(queryURL)})}})
