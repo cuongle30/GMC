836824859b1afbd3283b1683d5d43d533783c14f
cuongle30 <cuongle30@gmail.com>
2019-04-11 20:14:42 -0700

    update

--- javascript/app.js
+++ javascript/app.js
@@ -3,7 +3,7 @@ document.querySelector(".container").addEventListener("click", function(event) {
     // Variable to grab data from inside button
     var search = event.target.innerText
     // Constructing a queryURL using the animal name
-    var queryURL = `https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyCSWFckVRIzwf7VdUWd4EFtufS3Ma3Mp0&part=snippet,contentDetails,statistics,status`;
+    var queryURL = `https://www.googleapis.com/youtube/v3/videos?${search}&key=AIzaSyCSWFckVRIzwf7VdUWd4EFtufS3Ma3Mp0&part=snippet,contentDetails,statistics,status`;
     
     var movie = this.dataset.name;
     var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
@@ -16,4 +16,9 @@ document.querySelector(".container").addEventListener("click", function(event) {
       // After data comes back from the request
       .then(function(response) { return response.json() })
       .then(function(response) {
-        console.log(queryURL)})}})
+        console.log(queryURL);
+        console.log(response);
+      
+        var result = response.data;
+        
+      })}})
