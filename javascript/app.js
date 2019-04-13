563de20d4cd5daa7ba19ab8004da8d3ce18011d0
cuongle30 <cuongle30@gmail.com>
2019-04-13 13:34:11 -0700

    add-youtube

--- javascript/app.js
+++ javascript/app.js
@@ -20,7 +20,7 @@ document.getElementById("container").addEventListener("click", function (event) 
     var search = event.target.innerText
     var youtubeQueryURL = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${search}&type=video&videoCaption=closedCaption&key=AIzaSyDmKkf_-rWtH9yJ4insi91j9DWhxwj1e-o`
     var omdbQueryURL = `https://www.omdbapi.com/?apikey=d34a771e&t=${search}`
-    
+
 
     // Performing a request with the queryURL for youtube
     fetch(youtubeQueryURL, {
@@ -32,7 +32,14 @@ document.getElementById("container").addEventListener("click", function (event) 
       })
       .then(function (response) {
         //grab youtube videos from query
-        var youtubeVideos = response.items
+        document.addEventListener(
+          'DOMContentLoaded', () => setTimeout(initializeFreshchatWidget, 100)
+        )
+        console.log("youtube api responsed")
+        console.log(response)
+        
+        var youtubeVideos = response.items[0].id.videoId
+        ytplayer.loadVideoById({videoId: youtubeVideos})
         // Performing a request with the queryURL for OMDB
         fetch(omdbQueryURL, {
           method: "GET"
@@ -42,47 +49,59 @@ document.getElementById("container").addEventListener("click", function (event) 
           })
           .then(function (response) {
             //Grab OMDB data from query -- need to less specfic and not rating 
-           var omdbInfo = response.Ratings;
+            var omdbInfo = response.Ratings;
 
             console.log(youtubeVideos);
             console.log(omdbQueryURL)
             console.log(ombdInfo)
-          
 
-        var resultRender = document.querySelector("#result");
-        //Clear search
-        resultRender.innerHTML = "";
-        //OMDB for loop
 
-        var movieTitle = document.createElement("h3");
-          movieTitle.innerHTML = `Title: ${omdbInfo.Source}`;
-        for (let response of omdbInfo) {
-          console.log(response)
-          // Creating and storing a div tag
-          var suggestedDiv = document.createElement("div");
-          
-          // Creating a paragraph tag with the result item's rating
-          var p = document.createElement("p")
-          p.innerHTML = `Rating: ${response.Source} ${response.Value}`;
+            var resultRender = document.querySelector("#result");
+            //Clear search
+            resultRender.innerHTML = "";
+            //OMDB for loop
 
-          // Creating and storing an image tag
-          var resultImage = document.createElement("img");
-          
-          suggestedDiv.appendChild(p);
-          suggestedDiv.appendChild(resultImage);
-          resultRender.prepend(suggestedDiv)
-        };
+            var movieTitle = document.createElement("h3");
+            movieTitle.innerHTML = `Title: ${omdbInfo.Source}`;
+            for (let response of omdbInfo) {
+              console.log(response)
+              // Creating and storing a div tag
+              var suggestedDiv = document.createElement("div");
 
-        for (let response of omdbInfo) {
-          var resultVideo = document.createElement("video");
+              // Creating a paragraph tag with the result item's rating
+              var p = document.createElement("p")
+              p.innerHTML = `Rating: ${response.Source} ${response.Value}`;
 
-          resultVideo.innerHTML = response.id
+              // Creating and storing an image tag
+              var resultImage = document.createElement("img");
 
-          
+              suggestedDiv.appendChild(p);
+              suggestedDiv.appendChild(resultImage);
+              resultRender.prepend(suggestedDiv)
+            };
 
-        }
-
+          });
       });
-    });
   };
 });
+
+function video() {
+  console.log("video function call")
+var tag = document.createElement('script');
+tag.id = 'iframe-demo';
+tag.src = 'http://www.youtube.com/iframe_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+}
+
+//Create the YouTube Player.
+var ytplayer;
+function onYouTubeIframeAPIReady() {
+  console.log("onYouTubeIframeAPIReady")
+  ytplayer = new YT.Player('spherical-video-player', {
+    height: '300',
+    width: '440',
+  });
+}
+video();
+
