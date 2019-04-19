920120ac037ab9a3a45c6c40a049b9498a9ea400
cuongle30 <cuongle30@gmail.com>
2019-04-18 19:20:07 -0700

    clean-code

--- javascript/app.js
+++ javascript/app.js
@@ -1,68 +1,19 @@
 var youtubeVideos = [];
-var suggestedMovies = ["Avengers", "Get Smart"]
 var titleSearch = "";
-
 var year = "";
-
 // --- form validation ---
-
 $("#title-validation").hide();
-
-// function for created prepopulated buttons
-function createSuggestedButtons() {
-  var i;
-  for (i = 0; i < suggestedMovies.length; i++) {
-    var btn = document.createElement("BUTTON");
-    // setting and onlick event
-    btn.onclick = displayMovieInfo;
-    // button will show the suggested movie title
-    btn.innerHTML = suggestedMovies[i];
-    // Adding a class of movie-btn to our button
-    btn.classList.add("movie-btn");
-    btn.setAttribute("id", suggestedMovies[i]);
-    document.getElementById("suggested").appendChild(btn);
-  }
-}
-// run the function to created the suggested movie buttons
-createSuggestedButtons();
-
-// --- YouTube button ---
+// --- Return Suggested ---
 //Click event and query Youtube for recommended button title
 document.getElementById("suggested").addEventListener("click", function (event) {
   event.preventDefault();
-  var recommended = event.target.innerText + "official trailer"
-  var youtubeQueryURL = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${recommended}+${year}&key=AIzaSyDmKkf_-rWtH9yJ4insi91j9DWhxwj1e-o`
-  // create a function for performing a request with the queryURL for youtube
-  function youtubeFetch() {
-    fetch(youtubeQueryURL, {
-      method: "GET"
-    })
-      // Return JsonAfter data comes back from the request
-      .then(function (response) {
-        return response.json()
-      })
-      .then(function (response) {
-        //Settimeout to allow for response to pull
-        document.addEventListener(
-          'DOMContentLoaded', () => setTimeout(initializeFreshchatWidget, 100)
-        )
-        console.log("youtube api responded")
-        console.log(response)
-        //grab youtube videos from query
-        var youtubeVideos = response.items[0].id.videoId
-        ytplayer.loadVideoById({ videoId: youtubeVideos })
-      });
-  }
-  // call the youtubeFetch function
-
-  youtubeFetch();
+  var recommended = event.target.innerText
+  getData(recommended);
 });
-
-//  --- YoutTube search ---
+//  --- Return Searches ---
 //Click event and query YouTube for Search button
 document.getElementById("movie-search-btn").addEventListener("click", function (event) {
   event.preventDefault();
-
   // Variable to grab data from user search
   var titleSearch = document.getElementById("title-input").value.trim();
   // form validation
@@ -70,31 +21,10 @@ document.getElementById("movie-search-btn").addEventListener("click", function (
     $('#title-validation').slideDown("slow");
     $('#title-validation').slideUp(3000);
   } else {
-    var search = titleSearch + "official trailer"
-    
-    // Performing a request with the queryURL for youtube
-    setTimeout( function(){
-      var youtubeQueryURL = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${search}+${year}&key=AIzaSyDmKkf_-rWtH9yJ4insi91j9DWhxwj1e-o`
-    
-    fetch(youtubeQueryURL, {
-      method: "GET"
-    })
-      // Return JsonAfter data comes back from the request
-      .then(function (response) {
-        return response.json()
-      })
-      .then(function (response) {
-        //Settimeout to allow for response to pull
-        document.addEventListener(
-          'DOMContentLoaded', () => setTimeout(initializeFreshchatWidget, 250)
-        )
-        console.log("youtube api responded")
-        console.log(response)
-        //grab youtube videos from query
-        var youtubeVideos = response.items[0].id.videoId
-        ytplayer.loadVideoById({ videoId: youtubeVideos })
-      });
-    }, 300);
+    getData(titleSearch);
+    // display the results when someone does a search
+    $("#results").show();
+    shiftFocalPoint()
   }
 });
 
@@ -107,73 +37,30 @@ function video() {
   var firstScriptTag = document.getElementsByTagName('script')[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 }
-
 //run youtube function.
 var ytplayer;
 function onYouTubeIframeAPIReady() {
   console.log("onYouTubeIframeAPIReady")
   ytplayer = new YT.Player('spherical-video-player', {
-    height: '56%',
-    width: '85%',
+    height: '450px',
+    width: '700px',
   });
 }
 video();
 
-
-// --- OMDB buttons ---
-
 // OMDB search for suggested movies
 // displayMovieInfo function that is called when the button is clicked
 function displayMovieInfo() {
-
   // Only run code if the current element has the "movie-btn" class
   if (this.classList.contains("movie-btn")) {
     // Set the movie search to the button id that we set earlier 
     var movie = event.target.getAttribute("id");
-
-    // create the API search
-    var queryURL = `https://www.omdbapi.com/?t="${movie}&apikey=d34a771e`;
-
-    // Creating an AJAX call for the specific movie button being clicked
-    // Check if fetch is supported in the browser. If so use fetch 
-    if (window.fetch) {
-      fetch(queryURL, {
-        method: "GET"
-      })
-        .then((result) => result.json())
-        .then((response) => {
-          if (response.Error) {
-            console.error(response.Error);
-          } else {
-            year= response.Year;
-            renderMovieElements(response);
-          }
-        });
-    } else { // if fetch is not supported use XHR
-      const xhr = new XMLHttpRequest();
-      xhr.open("GET", queryURL);
-      xhr.onload = event => {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            let response = JSON.parse(xhr.response)
-            renderMovieElements(response);
-          } else {
-            console.error(xhr.responseText);
-          }
-        }
-      };
-      xhr.onerror = event => {
-        console.error(xhr.responseText);
-      };
-      xhr.send();
-    }
+    getData(movie);
   }
   // display the results when someone clicks the suggested movies button
   shiftFocalPoint()
   $("#results").show();
-  
 }
-
 // Function for building our ajax response
 function renderMovieElements(response) {
   // Creating a div to hold the movie
@@ -266,60 +153,6 @@ function renderMovieElements(response) {
   movieParent.replaceChild(movieDiv, movieParent.firstChild);
 }
 
-// --- OMDB Search --
-// this is the same as the Youtube -- consider combining
-document.getElementById("movie-search-btn").addEventListener("click", function (event) {
-  event.preventDefault();
-
-  // Variable to grab data from inside button
-  var titleSearch = document.getElementById("title-input").value.trim();
-
-  // data validation
-  if (titleSearch !== "") {
-    var queryURL = `https://www.omdbapi.com/?t="${titleSearch}&apikey=d34a771e`;
-
-    // Creating an AJAX call for the search
-    // Check if fetch is supported in the browser. If so use fetch 
-    if (window.fetch) {
-      fetch(queryURL, {
-        method: "GET"
-      })
-        .then((result) => result.json())
-        .then((response) => {
-          if (response.Error) {
-            console.error(response.Error);
-          } else {
-            year = response.Year
-            renderMovieElements(response);
-          }
-        });
-    } else { // if fetch is not supported use XHR
-      const xhr = new XMLHttpRequest();
-      xhr.open("GET", queryURL);
-      xhr.onload = event => {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            let response = JSON.parse(xhr.response)
-            renderMovieElements(response);
-          } else {
-            console.error(xhr.responseText);
-          }
-        }
-      };
-      xhr.onerror = event => {
-        console.error(xhr.responseText);
-      };
-      xhr.send();
-    }
-
-    // display the results when someone does a search
-    $("#results").show();
-
-    shiftFocalPoint () 
-
-  }
-});
-
 // --- UX/UI ---
 // --- to remove duplicate buttons ---
 // globally accessible empty firebase array to add to 
@@ -334,9 +167,9 @@ $("#suggested").hide();
 $("#suggested-toggle").click(function () {
   console.log("suggested movie has been clicked");
   // add everything in the suggested div to an array
-  $(".movie-btn").each(function(){
+  $(".movie-btn").each(function () {
     console.log("this id is: " + this.id);
-    firebaseMovies.splice(0,0, this.id);
+    firebaseMovies.splice(0, 0, this.id);
   })
   console.log("the firebaseMovies array: " + firebaseMovies);
   // capture only the unique values
@@ -348,20 +181,20 @@ $("#suggested-toggle").click(function () {
 });
 
 // function for shift focal point
-function shiftFocalPoint () {
+function shiftFocalPoint() {
   document.getElementById("enticing-image").style.height = "70px";
   $(".photo-credit").hide();
   $("#enticing-image").hide();
   document.getElementById("movie-search").removeAttribute("class");
-  document.getElementById("movie-search").setAttribute("class", "col-12 rounded" );
+  document.getElementById("movie-search").setAttribute("class", "col-12 rounded");
   document.getElementById("movie-search").style.position = "static";
 }
 
 //create a function to help remove duplcations pulled from: https://www.tutorialrepublic.com/faq/how-to-remove-duplicate-values-from-a-javascript-array.php
-function getUnique(array){
+function getUnique(array) {
   var uniqueArray = [];
-  for (i=0; i < array.length; i++) {
-    if(uniqueArray.indexOf(array[i]) === -1) {
+  for (i = 0; i < array.length; i++) {
+    if (uniqueArray.indexOf(array[i]) === -1) {
       uniqueArray.push(array[i]);
     }
   }
@@ -369,30 +202,24 @@ function getUnique(array){
 }
 
 // function to loop through unqie array and make buttons
-function addUniqueButtons (){
+function addUniqueButtons() {
   var i;
-  for (i=0; i < uniqueMovies.length; i++) {
-      let newBtn = document.createElement("BUTTON")
-      newBtn.innerHTML = uniqueMovies[i];
-      newBtn.onclick = displayMovieInfo;
+  for (i = 0; i < uniqueMovies.length; i++) {
+    let newBtn = document.createElement("BUTTON")
+    newBtn.innerHTML = uniqueMovies[i];
+    newBtn.onclick = displayMovieInfo;
 
-      newBtn.setAttribute("class", "movie-btn btn btn-outline-secondary");
+    newBtn.setAttribute("class", "movie-btn btn btn-outline-secondary");
 
-      newBtn.setAttribute("id", uniqueMovies[i]);
-      document.getElementById("suggested").appendChild(newBtn);
+    newBtn.setAttribute("id", uniqueMovies[i]);
+    document.getElementById("suggested").appendChild(newBtn);
   }
 }
 
 // function to clear existing buttons
 function clearExistingButtons() {
   $("#suggested").empty();
-  // var existingButtons = document.getElementById("suggested");
-  // while (existingButtons.hasChildNodes()) {
-  //   existingButtons.removeChild(existingButtons.firstChild);
-  // }
 }
-
-
 // hide the results area until someone clicks a button or searches for a movie
 $("#results").hide();
 
@@ -437,6 +264,7 @@ document.getElementById("movie-search-btn").addEventListener("click", function (
     document.getElementById("title-input").value = "";
   }
 })
+
 // create Firebase event for adding movie to the database
 database.ref().on("child_added", function (childSnapshot) {
   console.log(childSnapshot.val());
@@ -451,6 +279,7 @@ database.ref().on("child_added", function (childSnapshot) {
   let tempMovieData = {
     titleSearch: titleSearch
   };
+
   console.log(tempMovieData);
   // loop through the childSnapshot object and add buttons
   for (let prop of Object.values(tempMovieData)) {
@@ -461,4 +290,71 @@ database.ref().on("child_added", function (childSnapshot) {
     newBtn.setAttribute("id", prop);
     document.getElementById("suggested").appendChild(newBtn);
   }
-}) 
+})
+
+//Run Queries for OMDB and Youtube
+function getData(recommended) {
+  var queryURL = `https://www.omdbapi.com/?t="${recommended}&apikey=d34a771e`;
+  var youtubeQueryURL = "";
+  // Creating an AJAX call for the specific movie button being clicked
+  // Check if fetch is supported in the browser. If so use fetch 
+  if (window.fetch) {
+    fetch(queryURL, {
+      method: "GET"
+    })
+      .then((result) => result.json())
+      .then((response) => {
+        if (response.Error) {
+          console.error(response.Error);
+        } else {
+          year = response.Year;
+          renderMovieElements(response);
+          youtubeQueryURL = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${recommended + " official trailer"}+${year}&key=AIzaSyDmKkf_-rWtH9yJ4insi91j9DWhxwj1e-o`
+          youtubeFetch();
+        }
+      });
+  } else { // if fetch is not supported use XHR
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", queryURL);
+    xhr.onload = event => {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          let response = JSON.parse(xhr.response)
+          year = response.Year;
+          renderMovieElements(response);
+        } else {
+          console.error(xhr.responseText);
+        }
+      }
+    };
+    xhr.onerror = event => {
+      console.error(xhr.responseText);
+    };
+    xhr.send();
+  }
+
+  // create a function for performing a request with the queryURL for youtube
+  function youtubeFetch() {
+    fetch(youtubeQueryURL, {
+      method: "GET"
+    })
+      // Return JsonAfter data comes back from the request
+      .then(function (response) {
+        return response.json()
+      })
+      .then(function (response) {
+        //Settimeout to allow for response to pull
+        document.addEventListener(
+          'DOMContentLoaded', () => setTimeout(initializeFreshchatWidget, 100)
+        )
+        console.log("youtube api responded")
+        console.log(response)
+        //grab youtube videos from query
+        var youtubeVideos = response.items[0].id.videoId
+        ytplayer.loadVideoById({ videoId: youtubeVideos })
+      });
+  }
+}
+
+
+
